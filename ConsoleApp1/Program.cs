47d1970911e4ce0f3ffdcf1f9c8fb697847b1fad
mvlaic00 <56233262+mvlaic00@users.noreply.github.com>
2020-03-26 19:14:26 +0100

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,7 +7,9 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            // kopija console writeline
             Console.WriteLine("Hello World!");
         }
     }
 }
+//korištenjem shortcuta dupliciramo funkciju ispisa
